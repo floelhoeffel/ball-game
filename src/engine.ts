ac9267a4cb4acfbf963e8bd8062ae0e14367118b
Frederik Loelhoeffel <loewensprung@gmail.com>
2018-09-02 14:11:27 +0200

    Improved physics and render loop

--- src/engine.ts
+++ src/engine.ts
@@ -5,6 +5,9 @@ interface World {
   height: number
   acceleration: number
   friction: number
+  velXThreshold: number
+  velYThreshold: number
+  bounceFriction: number
 }
 
 export class Engine {
@@ -14,7 +17,10 @@ export class Engine {
     width: window.innerWidth,
     height: window.innerHeight,
     acceleration: 0.0012,
-    friction: 0.999
+    friction: 0.993,
+    velXThreshold: 0.01,
+    velYThreshold: 0.05,
+    bounceFriction: 0.7
   }
   constructor(private ball: Ball) {
     console.log("init engine")
@@ -34,42 +40,80 @@ export class Engine {
     let deltaTime = timestamp - this.lastTime
     this.lastTime = timestamp
 
+    // If dragged, end render loop
     if (this.ball.isDragged) {
       return
     }
 
-    this.ball.posY =
-      this.ball.posY +
-      deltaTime * (this.ball.velY + (deltaTime * this.world.acceleration) / 2)
+    // If no velocity, end render loop
+    if (this.ball.velX == 0 && this.ball.velY == 0) {
+      console.log("render loop ended")
 
-    this.ball.posX = this.ball.posX + deltaTime * this.ball.velX
+      return
+    }
 
+    // Only move if vel > 0
+    if (Math.abs(this.ball.velY) > 0) {
+      this.ball.posY =
+        this.ball.posY +
+        deltaTime * (this.ball.velY + (deltaTime * this.world.acceleration) / 2)
+    }
+
+    // Only move if vel > 0
+    if (Math.abs(this.ball.velX) > 0) {
+      this.ball.posX = this.ball.posX + deltaTime * this.ball.velX
+    }
+
+    // Friction
     this.ball.velX = this.ball.velX * this.world.friction
-    this.ball.velY = this.ball.velY + deltaTime * this.world.acceleration
+    if (
+      Math.abs(this.ball.velX) < this.world.velXThreshold &&
+      this.ball.velX != 0
+    ) {
+      this.ball.velX = 0
+      console.log("ball.velX=0")
+    }
 
-    // If ball would bleed out of screen
-    if (this.ball.posY + this.ball.height >= this.world.height) {
+    // Gravity, only if not laying on the ground
+    if (this.ball.posY !== this.world.height - this.ball.height) {
+      this.ball.velY = this.ball.velY + deltaTime * this.world.acceleration
+    }
+
+    // Bottom bounds
+    if (
+      this.ball.velY != 0 &&
+      this.ball.posY + this.ball.height >= this.world.height
+    ) {
+      console.log("bottom")
       this.ball.posY = this.world.height - this.ball.height
-      this.ball.velY = this.ball.velY * -0.7
+      this.ball.velY = this.ball.velY * this.world.bounceFriction * -1
 
       // If ball too slow, stop render loop
-      if (Math.abs(this.ball.velY) < 0.05) {
+      if (
+        Math.abs(this.ball.velY) < this.world.velYThreshold &&
+        Math.abs(this.ball.velY) > 0
+      ) {
         //world.acceleration = 0
-        //this.ball.setVelocityY(0)
-        console.log("killed")
-        return
+        console.log("ball.velY=0")
+        this.ball.velY = 0
+        //return
       }
     }
-
+    // Top bounds
     if (this.ball.posY <= 0) {
+      console.log("top")
       this.ball.posY = 0
-      this.ball.velY = this.ball.velY * -0.7
+      this.ball.velY = this.ball.velY * this.world.bounceFriction * -1
     }
-
+    // Right bounds
     if (this.ball.posX + this.ball.width > this.world.width) {
+      console.log("right")
+
       this.ball.velX = this.ball.velX * -1
     }
+    // Left bounds
     if (this.ball.posX <= 0) {
+      console.log("left")
       this.ball.velX = this.ball.velX * -1
     }
 
